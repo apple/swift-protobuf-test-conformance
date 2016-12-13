b1a75bd486310b1fa14af31718caec816f294b42
Thomas Van Lenten <thomasvl@google.com>
2016-12-13 10:34:11 -0500

    Conformance sources have moved to swift-protobuf repo

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-        name: "Conformance",
-        dependencies: [
-          .Package(url: "https://github.com/apple/swift-protobuf-runtime.git",
-                   Version(0,9,22))
-        ]
-)
